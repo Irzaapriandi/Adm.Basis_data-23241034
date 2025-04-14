c2b1fd6400d5f76b44768d56c6190179ce3a1511
Irzaapriandi <145946462+Irzaapriandi@users.noreply.github.com>
2025-04-14 10:35:52 +0800

    Add files via upload

---
+++ Pre-Praktikum/Pre-2.sql
@@ -0,0 +1,18 @@
+-- Nama : Irza Apriandi
+-- NIM : 23241034
+-- Kelas : A
+
+-- melihat database dalam MySQL
+SHOW DATABASES;
+
+-- memilih database yang digunakan
+USE mysql;
+
+-- menampilkan tabel dalam database mysql
+SHOW TABLES;
+
+-- Mendeskripsikan tabel user
+DESCRIBE user;
+
+-- Menampilkan isi data dari kolom Host, user, pasword
+SELECT Host, User, Password FROM user;
